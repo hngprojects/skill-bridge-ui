16e0002e9ea986c58f0d627b78e5dedab95129c6
leeyousuf <yesufulateefah@gmail.com>
2026-05-14 15:49:14 +0100

    feat(ui): change onboarding bg to white

--- src/app/(protected)/talent/onboarding/layout.tsx
+++ src/app/(protected)/talent/onboarding/layout.tsx
@@ -2,7 +2,7 @@ import { AuthShell } from "@/components/custom/auth-shell";
 import React, { ReactNode } from "react";
 
 const OnboardingLayout = ({ children }: { children: ReactNode }) => {
-  return <AuthShell>{children}</AuthShell>;
+  return <AuthShell className="bg-white">{children}</AuthShell>;
 };
 
 export default OnboardingLayout;
