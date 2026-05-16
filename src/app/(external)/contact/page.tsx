e02d05ab95140157cc694c69b9bc183b886a2a04
Mal. Faruk Dakta <81782489+rukuboy1997@users.noreply.github.com>
2026-05-16 09:52:25 +0000

    fix: added contact us main page

---
+++ src/app/(external)/contact/page.tsx
@@ -0,0 +1,13 @@
+import { ContactForm } from "./contact-form";
+import { HeroSection } from "./contact-hero";
+import { ReachOut } from "./reach-out";
+
+export default function ContactPage() {
+  return (
+    <div>
+      <HeroSection />
+      <ContactForm />
+      <ReachOut />
+    </div>
+  );
+}
