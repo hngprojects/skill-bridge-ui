fde79eb42435d9b6391253693ea43186d73b4646
Moluno-xiii <molunoprogress@gmail.com>
2026-05-19 19:28:59 +0100

    fix: remove extra margin on assement-container to remove vertical scroll on laptop screens

--- src/components/assessments/assessment-container.tsx
+++ src/components/assessments/assessment-container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const AssessmentContainer = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="border border-[#DBDBDB] max-w-238 lg:w-full mx-auto my-20 rounded-4xl text-[#151515] bg-white flex flex-col lg:p-6 px-2.5 py-6 max-lg:mx-3">
+    <div className="border border-[#DBDBDB] max-w-238 lg:w-full mx-auto my-10 rounded-4xl text-[#151515] bg-white flex flex-col md:p-6 px-2.5 py-6 max-lg:mx-3">
       {children}
     </div>
   );
