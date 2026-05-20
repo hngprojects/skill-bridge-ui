ab12becad931fad579a6e3350dbfd88e2455692e
lateefaayesufu <lateefaayesufu@gmail.com>
2026-05-20 23:20:16 +0100

    feat: fix path error

--- src/app/(protected)/t/assessments/verified-report/page.tsx
+++ src/app/(protected)/t/assessments/verified-report/page.tsx
@@ -1,4 +1,4 @@
-import VerifiedReportPage from "@/components/assessments/verified-report-page";
+import VerifiedReportPage from "@/components/verified-report/verified-report-page";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
