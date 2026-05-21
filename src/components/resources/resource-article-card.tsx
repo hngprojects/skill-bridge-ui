ef70a284da03892cc4765d20492f94d496904dd6
lateefaayesufu <lateefaayesufu@gmail.com>
2026-05-21 12:44:22 +0100

    feat(resources): implement article card with icon, title, description and read time

---
+++ src/components/resources/resource-article-card.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import { ResourceArticle } from "@/constants/resources";
+
+const ResourceArticleCard = ({
+  title,
+  description,
+  readTime,
+}: ResourceArticle) => {
+  return (
+    <div className="flex flex-col flex-1 bg-muted border border-border rounded-2xl p-4 gap-y-4 min-w-0">
+      <Image
+        src="/assets/resources/resources-article-icon.svg"
+        alt="article icon"
+        width={64}
+        height={64}
+      />
+      <div className="flex flex-col gap-y-3">
+        <div className="flex flex-col gap-y-2">
+          <p className="label text-foreground">{title}</p>
+          <p className="body font-light text-foreground">{description}</p>
+        </div>
+        <p className="body-2 text-muted-foreground">{readTime}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ResourceArticleCard;
