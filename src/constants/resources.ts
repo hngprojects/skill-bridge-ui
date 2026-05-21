1d9064bb536ae4b3d26790bcc66cb5f5e13f1b3d
lateefaayesufu <lateefaayesufu@gmail.com>
2026-05-21 23:30:31 +0100

    fix(resources): move resource data to constants folder

--- src/constants/resources.ts
+++ src/constants/resources.ts
@@ -1,30 +1,4 @@
-type ResourceArticle = {
-  id: number;
-  title: string;
-  description: string;
-  readTime: string;
-};
-
-type ResourceVideo = {
-  id: number;
-  title: string;
-  duration: string;
-  thumbnail: string;
-};
-
-type ResourceArticleSection = {
-  title: string;
-  type: "article";
-  items: ResourceArticle[];
-};
-
-type ResourceVideoSection = {
-  title: string;
-  type: "video";
-  items: ResourceVideo[];
-};
-
-type ResourceSection = ResourceArticleSection | ResourceVideoSection;
+import type { ResourceSection } from "@/types/resources";
 
 const resourceSections: ResourceSection[] = [
   {
@@ -107,5 +81,4 @@ const resourceSections: ResourceSection[] = [
   },
 ];
 
-export type { ResourceSection, ResourceArticle, ResourceVideo };
 export { resourceSections };
