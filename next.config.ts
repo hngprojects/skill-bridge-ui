a5f7c6486847a2997a9473fdcbf15bac72f60242
Ayomikun <ayoadeosun10@gmail.com>
2026-05-22 23:35:40 +0100

    chore: add remote image patterns to next config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,20 @@ const nextConfig: NextConfig = {
   experimental: {
     authInterrupts: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.staging.skillbridge.hng14.com",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.skillbridge.hng14.com",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
