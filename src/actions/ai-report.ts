93b931ee422b6478e4edc3bc1e611cc8b15cc9d7
Moluno-xiii <molunoprogress@gmail.com>
2026-05-28 23:49:11 +0100

    fix: add consistent typing across ai-report api response

--- src/actions/ai-report.ts
+++ src/actions/ai-report.ts
@@ -1,13 +1,13 @@
-import { GuidanceReport } from "@/types/resources";
+import { AIReportResponse } from "@/types/resources";
 import { unwrapData } from "@/actions";
 import { authApi } from "@/lib/api";
 import { ApiEnvelope } from "@/types/api";
 
 const getAiReport = async () => {
-  const response = await authApi.get<ApiEnvelope<GuidanceReport>>(
+  const response = await authApi.get<ApiEnvelope<AIReportResponse>>(
     "/talent/ai-report/guidance-report",
   );
-  return unwrapData(response);
+  return unwrapData(response).advanced_guidance_report;
 };
 
 export default getAiReport;
